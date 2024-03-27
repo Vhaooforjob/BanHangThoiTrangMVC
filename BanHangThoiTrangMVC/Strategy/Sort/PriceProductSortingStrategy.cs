bfdf41cf0fd748886b64dec2934054566131ad96
VHAOO\Vhaoo <vhaooforjob@gmail.com>
2024-03-27 10:08:25 +0700

    apply using strategy change sort

---
+++ BanHangThoiTrangMVC/Strategy/Sort/PriceProductSortingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanHangThoiTrangMVC.Strategy.Sort
+{
+    public class PriceProductSortingStrategy : IProductSortingStrategy
+    {
+        public PriceProductSortingStrategy() { }
+        public IQueryable<ProductViewModel> SortProducts(IQueryable<ProductViewModel> products)
+        {
+            return products.OrderBy(p => p.Price);
+        }
+    }
+}
